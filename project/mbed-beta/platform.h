d86d85be2c0020e98f1289b3f50840189baeef7d
Ethan Chen <intervigil@gmail.com>
2011-11-23 01:45:51 -0800

    don't ignore mbed beta library anymore

---
+++ project/mbed-beta/platform.h
@@ -0,0 +1,14 @@
+/* mbed Microcontroller Library - platform
+ * Copyright (c) 2009 ARM Limited. All rights reserved.
+ */ 
+ 
+#ifndef MBED_PLATFORM_H
+#define MBED_PLATFORM_H
+
+#if defined(TARGET_LPC1768) || defined(TARGET_LPC2368)
+#define MBED_RPC
+#endif
+
+#define MBED_OPERATORS
+
+#endif
