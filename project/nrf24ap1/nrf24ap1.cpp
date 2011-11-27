7403f0097db1b5e7b94d4eee7e894d654213d92a
Ethan Chen <intervigil@gmail.com>
2011-11-27 01:46:03 -0800

    skeletons of functions for nrf24ap1 class

--- project/nrf24ap1/nrf24ap1.cpp
+++ project/nrf24ap1/nrf24ap1.cpp
@@ -1,1 +1,118 @@
+#include "mbed.h"
 #include "nrf24ap1.h"
+
+
+namespace {
+
+uint8_t get_checksum(uint8_t *buf, int len) {
+  uint8_t res = 0;
+
+  for (int i = 0; i < len; i++) {
+    res ^= buf[i];
+  }
+  return res;
+}
+
+void inline send_packet(Serial *port, uint8_t *packet, int len) {
+  for (int i = 0; i < len; i++) {
+    port->putc(packet[i]);
+  }
+}
+
+void send_assign_channel(Serial *port, int chan_id) {
+  uint8_t packet[7];
+  packet[0] = MESG_TX_SYNC;
+  packet[1] = 3;
+  packet[2] = MESG_ASSIGN_CHANNEL_ID;
+  packet[3] = chan_id;
+  packet[4] = chan_type;
+  packet[5] = network_num;
+  packet[6] = get_checksum(&packet, 6);
+  send_packet(port, &packet, 7);
+}
+
+void send_unassign_channel(Serial *port, int chan_id) {
+  uint8_t packet[5];
+  packet[0] = MESG_TX_SYNC;
+  packet[1] = 1;
+  packet[2] = MESG_UNASSIGN_CHANNEL_ID;
+  packet[3] = chan_id;
+  packet[4] = get_checksum(&packet, 4);
+  send_packet(port, &packet, 5);
+}
+
+void send_set_channel_id(Serial *port, int chan_id) {
+  uint8_t packet[9];
+  packet[0] = MESG_TX_SYNC;
+  packet[1] = 5;
+  packet[2] = MESG_CHANNEL_ID_ID;
+  packet[3] = chan_id;
+  packet[4] = dev_num0;
+  packet[5] = dev_num1;
+  packet[6] = dev_type_id;
+  packet[7] = trans_type;
+  packet[8] = get_checksum(&packet, 8);
+  send_packet(port, &packet, 9);
+}
+
+void send_open_channel(Serial *port, int chan_id) {
+  uint8_t packet[5];
+  packet[0] = MESG_TX_SYNC;
+  packet[1] = 1;
+  packet[2] = MESG_OPEN_CHANNEL_ID;
+  packet[3] = chan_id;
+  packet[4] = get_checksum(&packet, 4);
+  send_packet(port, &packet, 5);
+}
+
+}
+
+namespace Nrf24ap1 {
+
+Nrf24ap1::Nrf24ap1(PinName tx, PinName rx, PinName ctx) {
+  ap1_ = new Serial(tx, rx);
+  cts_pin_ = new InterruptIn(ctx);
+  ap1_->attach(this, &HandleMessage, Serial:RxIrq);
+  ap1_->baud(NRF24AP1_BAUD);
+}
+
+void Nrf24ap1::Reset() {
+  uint8_t packet[5];
+  packet[0] = MESG_TX_SYNC;
+  packet[1] = 1;
+  packet[2] = MESG_SYSTEM_RESET_ID;
+  packet[3] = 0;
+  packet[4] = get_checksum(&packet, 4);
+  send_packet(port, &packet, 5);
+}
+
+int Nrf24ap1::OpenChannel(int chan_id) {
+  // assign channel
+  wait_ms(50);
+  // set channel id
+  wait_ms(50);
+  // open channel
+
+  channels_.push_back(chan_id);
+}
+
+void Nrf24ap1::CloseChannel(int chan_id) {
+
+}
+
+int Nrf24ap1::Send(int channel_id, uint8_t *buf, int len) {
+  // use BroadcastData to push data to channel_id
+
+}
+
+void Nrf24ap1::HandleMessage() {
+  // check what data is in serial
+  // build data, send it to rx_handler if complete message
+}
+
+Nrf24ap1::~Nrf24ap1() {
+  delete ap1_;
+  delete cts_pin_;
+}
+
+}
--- project/nrf24ap1/nrf24ap1.h
+++ project/nrf24ap1/nrf24ap1.h
@@ -1,4 +1,36 @@
 #ifndef NRF24AP1_H_
 #define NRF24AP1_H_
 
+#include <list>
+
+#include "libant.h"
+#include "mbed.h"
+
+
+#define NRF24AP1_BAUD 4800 // default Sparkfun ANT Baud
+
+namespace Nrf24ap1 {
+
+class Nrf24ap1 {
+ public:
+  Nrf24ap1(PinName tx, PinName rx, PinName cts);
+  void Reset();
+  int OpenChannel(int chan_id);
+  void CloseChannel(int chan_id);
+  int Send(uint8_t *buf, int len);
+  void SetReceiveHandler(void (*handler)(int, uint8_t *, int));
+  ~Nrf24ap1();
+
+ protected:
+  void HandleMessage();
+
+ private:
+  Serial *ap1_;
+  InterruptIn *cts_pin_;
+  std::list<int> channels_;
+  void (*rx_handler_)(int, uint8_t *, int); // channel, data, len
+};
+
+}
+
 #endif
