cccbd374adb0a0b94fbe22033736294505633125
Ethan Chen <intervigil@gmail.com>
2011-11-30 20:13:00 -0800

    this isn't Java

--- project/synced_sd_filesystem.cpp
+++ project/synced_sd_filesystem.cpp
@@ -5,28 +5,28 @@ SyncedSDFileSystem::SyncedSDFileSystem(PinName mosi, PinName miso, PinName sclk,
 }
 
 int SyncedSDFileSystem::disk_initialize() {
-  // do sync stuff, then do superclass disk_initialize
-  return super::disk_initialize();
+  // do sync stuff, then do SyncedSDFileSystemclass disk_initialize
+  return SyncedSDFileSystem::disk_initialize();
 }
 
 int SyncedSDFileSystem::disk_write(const char *buffer, int block_number) {
-  return super::disk_write(buffer, block_number);
+  return SyncedSDFileSystem::disk_write(buffer, block_number);
 }
 
 int SyncedSDFileSystem::disk_read(char *buffer, int block_number) {
-  return super::disk_read(buffer, block_number);
+  return SyncedSDFileSystem::disk_read(buffer, block_number);
 }
 
 int SyncedSDFileSystem::disk_status() {
-  return super::disk_status();
+  return SyncedSDFileSystem::disk_status();
 }
 
 int SyncedSDFileSystem::disk_sync() {
-  return super::disk_sync();
+  return SyncedSDFileSystem::disk_sync();
 }
 
 int SyncedSDFileSystem::disk_sectors() {
-  return super::disk_sync();
+  return SyncedSDFileSystem::disk_sync();
 }
 
 // PRIVATE FUNCTIONS
