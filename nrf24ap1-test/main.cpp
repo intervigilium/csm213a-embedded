8d5b9d86fb33a9ba765f3f3008ae601cecc6ba6a
Ethan Chen <intervigil@gmail.com>
2011-12-07 00:49:51 -0800

    fix test program driver for ap1_packet send/receive

--- nrf24ap1-test/main.cpp
+++ nrf24ap1-test/main.cpp
@@ -5,54 +5,48 @@
 #define CHANNEL_ID 1
 #define NUM_ITERATIONS 10
 
-void on_master_receive(uint8_t type, uint8_t *data, int len) {
-  switch (type) {
-    case MESG_BROADCAST_DATA_ID:
-      printf("MASTER RECEIVED MESSAGE: ");
-      for (int i = 0; i < len; i++) {
-        printf("%c", data[i]);
-      }
-      printf("\n\r");
-      break;
-    default:
-      printf("MASTER RECEIVED MESSAGE TYPE: 0x%x\n\r", type);
-      break;
-  }
+struct ap1_packet * create_ap1_packet(int len) {
+  struct ap1_packet *p = (struct ap1_packet *) malloc(sizeof(struct ap1_packet));
+  p->data = (uint8_t *) malloc(sizeof(uint8_t) * len);
+  p->length = len;
+  return p;
 }
 
-void on_slave_receive(uint8_t type, uint8_t *data, int len) {
-  switch (type) {
-    case MESG_BROADCAST_DATA_ID:
-      printf("SLAVE RECEIVED MESSAGE: ");
-      for (int i = 0; i < len; i++) {
-        printf("%c", data[i]);
-      }
-      printf("\n\r");
-      break;
-    default:
-      printf("SLAVE RECEIVED MESSAGE TYPE: 0x%x\n\r", type);
-      break;
-  }
+void free_ap1_packet(struct ap1_packet *p) {
+  free(p->data);
+  free(p);
+}
+
+void on_master_receive(struct ap1_packet *p) {
+  printf("MASTER RECEIVED MESSAGE: %s\n\r", p->data);
+}
+
+void on_slave_receive(struct ap1_packet *p) {
+  printf("SLAVE RECEIVED MESSAGE: %s\n\r", p->data);
 }
 
 void do_master(Nrf24ap1::Nrf24ap1 *ap1) {
   printf("MASTER\n\r");
-  char buf[13] = "pingpingping";
+  struct ap1_packet *p = create_ap1_packet(13);
+  sprintf((char *) p->data, "pingpingping");
   ap1->SetReceiveHandler(&on_master_receive);
   ap1->OpenChannel(CHANNEL_ID, TX_DUPLEX_CHANNEL_TYPE);
   for (int i = 0; i < NUM_ITERATIONS; i++) {
-    ap1->Send(CHANNEL_ID, (uint8_t *) buf, 13);
+    ap1->Send(CHANNEL_ID, p);
   }
+  free_ap1_packet(p);
 }
 
 void do_slave(Nrf24ap1::Nrf24ap1 *ap1) {
   printf("SLAVE\n\r");
-  char buf[13] = "pongpongpong";
+  struct ap1_packet *p = create_ap1_packet(13);
+  sprintf((char *) p->data, "pongpongpong");
   ap1->SetReceiveHandler(&on_slave_receive);
   ap1->OpenChannel(CHANNEL_ID, RX_DUPLEX_CHANNEL_TYPE);
   for (int i = 0; i < NUM_ITERATIONS; i++) {
-    ap1->Send(CHANNEL_ID, (uint8_t *) buf, 13);
+    ap1->Send(CHANNEL_ID, p);
   }
+  free_ap1_packet(p);
 }
 
 int main() {
