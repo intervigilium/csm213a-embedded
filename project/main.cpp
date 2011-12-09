539f128f81a50037dcf0f31b9d20676f9e9a41c0
Ethan Chen <intervigil@gmail.com>
2011-12-08 21:31:01 -0800

    build and include lwip

--- project/main.cpp
+++ project/main.cpp
@@ -1,5 +1,20 @@
-#include "sdfs/SDFileSystem.h"
+#include "lwip/opt.h"
+#include "lwip/stats.h"
+#include "lwip/sys.h"
+#include "lwip/pbuf.h"
+#include "lwip/udp.h"
+#include "lwip/tcp.h"
+#include "lwip/dns.h"
+#include "lwip/dhcp.h"
+#include "lwip/init.h"
+#include "lwip/netif.h"
+#include "lwip_device.h"
 #include "mbed.h"
+#include "netif/etharp.h"
+#include "netif/loopif.h"
+#include "sdfs/SDFileSystem.h"
+
+Ethernet eth0;
 
 
 int main() {
