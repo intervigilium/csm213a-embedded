c88995c00d157567b926140a8e8a9d2a380d5b09
Ethan Chen <intervigil@gmail.com>
2011-12-08 22:03:43 -0800

    Revert "build and include lwip"
    This reverts commit 539f128f81a50037dcf0f31b9d20676f9e9a41c0.

--- project/main.cpp
+++ project/main.cpp
@@ -1,20 +1,5 @@
-#include "lwip/opt.h"
-#include "lwip/stats.h"
-#include "lwip/sys.h"
-#include "lwip/pbuf.h"
-#include "lwip/udp.h"
-#include "lwip/tcp.h"
-#include "lwip/dns.h"
-#include "lwip/dhcp.h"
-#include "lwip/init.h"
-#include "lwip/netif.h"
-#include "lwip_device.h"
-#include "mbed.h"
-#include "netif/etharp.h"
-#include "netif/loopif.h"
 #include "sdfs/SDFileSystem.h"
-
-Ethernet eth0;
+#include "mbed.h"
 
 
 int main() {
